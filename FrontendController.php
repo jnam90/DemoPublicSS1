4dfc6f72266b72da32112191d7d392630df858a0
Nhut Nam <tran.le.nhut.nam@gmail.com>
2019-02-25 17:59:37 +0700

    no messages

--- FrontendController.php
+++ FrontendController.php
@@ -16,47 +16,39 @@ class FrontendController extends Controller
 
     public function register(Request $request)
     {
-        if ($request->isMethod('post')) {
-            $validator = Validator::make($request->all(), ['firstname' => 'required', 'familyname' => 'required', 'email' => 'required|email',]);
-            if ($validator->fails()) {
-                    return redirect()->route('register')
-                                ->withErrors($validator)
-                                ->withInput();
-            }
-
-            $checkEmail = Doctor::checkEmailExist($request->email); 
-            if($checkEmail) { 
-                $validator->errors()->add('error', 'Email registed!');
-                return redirect()->route('register')
-                            ->withErrors($validator)
-                            ->withInput(); 
-            }
-
-            $verification_code = Doctor::generateCode();
-            $data = [
-                'firstname' => $request->firstname,
-                'familyname' => $request->familyname,
-                'email' => $request->email,
-                'status' => 0,
-                'verification_code' => $verification_code
-
-            ];
-            $create = Doctor::create($data); 
-            if($create) {
-                Mail::to($request->email)->send(new VerificationCode(['code' => $verification_code ])); 
-                session()->put('email', $request->email);   
-                return redirect()->route('verify');  
-                     
-            } 
-            else {
-                $validator->errors()->add('error', 'Register failed!');
+        if (!$request->isMethod('post')) {
+             return view('register')
+        } 
+        $validator = Validator::make($request->all(), ['firstname' => 'required', 'familyname' => 'required', 'email' => 'required|email',]);
+        if ($validator->fails()) {
                 return redirect()->route('register')
                             ->withErrors($validator)
                             ->withInput();
-            }
- 
+        }
+
+        $checkEmail = Doctor::checkEmailExist($request->email); 
+        if($checkEmail) { 
+            $validator->errors()->add('error', 'Email registed!');
+            return redirect()->route('register')
+                        ->withErrors($validator)
+                        ->withInput(); 
+        }
+
+        $verification_code = Doctor::generateCode();
+        $data = ['firstname' => $request->firstname,'familyname' => $request->familyname,'email' => $request->email,       'status' => 0,           'verification_code' => $verification_code ];
+        $create = Doctor::create($data); 
+        if($create) {
+            Mail::to($request->email)->send(new VerificationCode(['code' => $verification_code ])); 
+            session()->put('email', $request->email);   
+            return redirect()->route('verify');  
+
         } 
-        return view('register'); 
+        else {
+            $validator->errors()->add('error', 'Register failed!');
+            return redirect()->route('register')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
     }
     
     public function beforeandafterPhotocontest(Request $request) {
